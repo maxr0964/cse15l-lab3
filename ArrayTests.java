e4654ec2488e7d5f937e676cecb4d7fa8f68b0d3
Max Rosenbloom <mrosenbloom@ucsd.edu>
2023-01-26 15:19:22 -0800

    doing lab things

--- ArrayTests.java
+++ ArrayTests.java
@@ -7,6 +7,10 @@ public class ArrayTests {
     int[] input1 = { 3 };
     ArrayExamples.reverseInPlace(input1);
     assertArrayEquals(new int[]{ 3 }, input1);
+
+    int[] input2 = {1, 2, 3, 4, 5};
+    ArrayExamples.reverseInPlace(input2);
+    assertArrayEquals(new int[]{5, 4, 3, 2, 1}, input2);
 	}
 
 
@@ -14,5 +18,22 @@ public class ArrayTests {
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+    int[] input2 = {1, 2, 3, 4};
+    int[] input2reversed = {4, 3, 2, 1};
+    assertArrayEquals(input2reversed, ArrayExamples.reversed(input2));
+
+  }
+
+  @Test
+  public void testAverageWithoutLowest() {
+    double[] testerArray = {1, 2, 3, 4};
+    assertEquals(3.0, ArrayExamples.averageWithoutLowest(testerArray), 0.001);
+
+    double[] testerArray2 = {1,1,1,1};
+    assertEquals(1.0, ArrayExamples.averageWithoutLowest(testerArray2), 0.001);
+  
+    double[] testerArray3 = {3,2,1,2,3};
+    assertEquals(2.5, ArrayExamples.averageWithoutLowest(testerArray3), 0.001);
+  
   }
 }
